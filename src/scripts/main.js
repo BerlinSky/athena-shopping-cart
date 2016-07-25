f20ce85104246604905a5a1428353b54800c3b89
Berlin Sky <pedroyoung8989@gmail.com>
2016-07-25 12:32:36 -0700

    Add select control

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,31 +1,37 @@
 import $ from 'jquery';
 import Cleave from 'cleave.js';
 // require('cleave.js/dist/addons/cleave-phone.{country}');
-import 'chosen';
+// import 'chosen';
+import Select2 from 'select2';
 
 $(function () {
 
   $('.js-credit--default').css({ "display": "block" });
 
-	const cleaveCardNum = new Cleave('.js-dataInput--cardNum', {
-    creditCard: true,
-    onCreditCardTypeChanged: function (type) {
-      console.log(type);
+  if ($('.js-dataInput--cardNum').length > 0) { 
+    // it exists 
+		const cleaveCardNum = new Cleave('.js-dataInput--cardNum', {
+		  creditCard: true,
+		  onCreditCardTypeChanged: function (type) {
+		    console.log(type);
 
-      if (type === 'unknown') {
-	      dsiplayCardImages("default");
-      }
-      else {
-	      dsiplayCardImages(type);
-      }
-    }
-	});
+		    if (type === 'unknown') {
+		      dsiplayCardImages("default");
+		    }
+		    else {
+		      dsiplayCardImages(type);
+		    }
+		  }
+		});
+	}
 
-	const cleaeveCardDate = new Cleave('.js-dataInput--cardExpDate', {
-    date: true,
-    datePattern: ['m', 'Y'],
-    delimiter: '/'
-	});
+  if ($('.js-dataInput--cardExpDate').length > 0) { 
+		const cleaeveCardDate = new Cleave('.js-dataInput--cardExpDate', {
+	    date: true,
+	    datePattern: ['m', 'Y'],
+	    delimiter: '/'
+		});
+	}
 
 	function dsiplayCardImages(type) {
     $('.js-credit--amex').hide();
@@ -49,28 +55,35 @@ $(function () {
 	  } 
 	});
 
-	$(".js-selectCountry").chosen({
-		max_selected_options: 1,
-		width: "200px"
-	})
+	$(".js-selectCountry").select2();
 
-	$(".js-selectState").chosen({
-		max_selected_options: 1,
-		width: "150px"
-	})
+	$(".js-selectState").select2();
+
+	
+
+
+	// $(".js-selectCountry").chosen({
+	// 	max_selected_options: 1,
+	// 	width: "200px"
+	// })
+
+	// $(".js-selectState").chosen({
+	// 	max_selected_options: 1,
+	// 	width: "150px"
+	// })
 		
-	$('.chosen-container.chosen-container-single').css(
-		{	
-			// "width": "240px", 
-			"font-size": "18px",
-			"background-color": "#fff",
-			"border": "1px solid #eee"
-		}
-	);
+	// $('.chosen-container.chosen-container-single').css(
+	// 	{	
+	// 		// "width": "240px", 
+	// 		"font-size": "18px",
+	// 		"background-color": "#fff",
+	// 		"border": "1px solid #eee"
+	// 	}
+	// );
 
-	$('.chosen-single').css(
-		{"border": 0, "border-radius": 0}
-	);
+	// $('.chosen-single').css(
+	// 	{"border": 0, "border-radius": 0}
+	// );
 
 });
 
