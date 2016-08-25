1267b7b6672edde5964ad0003480b1b58b55f2fd
Berlin Sky <pedroyoung8989@gmail.com>
2016-08-25 09:44:01 -0700

    Update UI

---
+++ src/scripts/data/books-data.js
@@ -0,0 +1,58 @@
+export const books = [
+    {
+        isbn: '0446310786',
+        author: 'Haper Lee',
+        title: 'To Kill a Mockingbird',
+        publisher: 'Grand Central Publishing',
+        pubdate: 'October 11, 1988',
+        price: '17.33'
+    },
+    {
+        isbn: '0141439556',
+        author: 'Emily Bronte',
+        title: 'Wuthering Heights',
+        publisher: 'Penguin Classic',
+        pubdate: 'December 31, 2002',
+        price: '12.99'
+    },
+    {
+        isbn: '0684801221',
+        author: 'Ernest Hemingway',
+        title: 'The Old Man and The Sea',
+        publisher: 'Scribner',
+        pubdate: 'May 5, 1995',
+        price: '15.28'
+    },
+    {
+        isbn: '0316769487',
+        author: 'J.D. Salinger',
+        title: 'The Catcher in the Rye',
+        publisher: 'Little, Brown and Company',
+        pubdate: 'May 1, 1991',
+        price: '15.28'
+    },
+    {
+        isbn: '1451673310',
+        author: 'Ray Bradbury',
+        title: 'Fahrenheit 451',
+        publisher: 'Simon & Schuster',
+        pubdate: 'June 1, 2013',
+        price: '15.69'
+    },
+    {
+        isbn: '0451524934',
+        author: 'Geroge Orwell',
+        title: '1984',
+        publisher: 'Signet Classic',
+        pubdate: 'January 1, 1961',
+        price: '15.75'
+    },
+    {
+        isbn: '0140177396',
+        author: 'John Steinbeck',
+        title: 'Of Mice and Men',
+        publisher: 'Penguin Books',
+        pubdate: 'September 1, 1993',
+        price: '18.99'
+    }
+];
